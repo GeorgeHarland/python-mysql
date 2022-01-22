31f1febfffdbaffda588f7c1f09d08f3e10c1c4d
George Harland <georgeharland12@gmail.com>
2022-01-22 23:58:32 +0000

    Table creation

--- tryingsql.py
+++ tryingsql.py
@@ -15,6 +15,7 @@ except Exception as e:
 
 # creating a database
 try:
+    # Set command handler
     ch = db.cursor()
     ch.execute("CREATE DATABASE fruits")
     print("Database has been created.")
@@ -41,3 +42,10 @@ except Exception as e:
     print("Could not connect to the %s database." % db_to_connect)
 
 # creating tables in a database
+try:
+    ch = db1.cursor()
+    ch.execute("CREATE TABLE YellowFruit (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20), avr_weight FLOAT)")
+    print("Table created.")
+except Exception as e:
+    print("Table creation failed.")
+    print(e)
